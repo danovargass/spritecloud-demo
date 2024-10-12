1ed72d47f87156bce6de1ebd532bcac6358045fe
dvargas <danovargas@gmail.com>
2024-10-11 20:34:08 -0600

    config and package.json updates

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,5 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices, PlaywrightTestConfig } from '@playwright/test';
+import baseEnvUrl from './envBaseURL';
 
 /**
  * Read environment variables from file.
@@ -12,7 +13,8 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
+  //commented to run all tests found 
+  //testDir: '.tests/',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -48,6 +50,15 @@ export default defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
+    {
+      name: 'apiUrl',
+      use: { baseURL: baseEnvUrl.api.apiEndpoint, },
+
+    },
+    {
+      name: 'uiUrl',
+      use: { baseURL: baseEnvUrl.ui.uiEndpoint, }
+    }
 
     /* Test against mobile viewports. */
     // {
@@ -69,7 +80,6 @@ export default defineConfig({
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
-
   /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
